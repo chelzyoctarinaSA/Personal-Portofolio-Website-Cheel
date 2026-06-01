483965f4dcdaa73e247d8840a42fddfdfd7df21a
chelzyoctarinaSA <chelzyoctarina06@gmail.com>
2026-06-01 23:25:18 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
