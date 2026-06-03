669181c52c770c85c1961fce8ee12c2dce42a2d7
chelzyoctarina06 <chelzyoctarina06@gmail.com>
2026-06-04 03:11:25 +0800

    first commit

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 });
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.heading', { origin: 'top' });
+
+ScrollReveal().reveal('.home-img, .skills-container, .service-container, .contact form', { origin: 'bottom' });
+
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
