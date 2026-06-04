2081afa1e18ea8a7fb329788963148104880ca26
chelzyoctarina06 <chelzyoctarina06@gmail.com>
2026-06-04 21:18:36 +0800

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links a');
 
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    });
 });
 
 ScrollReveal({
@@ -14,9 +22,6 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.heading', { origin: 'top' });
-
 ScrollReveal().reveal('.home-img, .skills-container, .service-container, .contact form', { origin: 'bottom' });
-
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
