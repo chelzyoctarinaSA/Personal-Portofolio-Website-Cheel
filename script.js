2937b05589b1f37b7e009c188a75b0a7f6575916
chelzyoctarina06 <chelzyoctarina06@gmail.com>
2026-06-05 14:32:59 +0800

    first commit

--- script.js
+++ script.js
@@ -25,3 +25,4 @@ ScrollReveal().reveal('.heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .skills-container, .service-container, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
